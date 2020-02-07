006a6f8b7bcd0180e0a141758a5754dad350429f
fairliereese <fairliek@comcast.net>
2020-02-07 09:52:34 -0800

    renaming stuff

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import setup
+setup(
+  name = 'swan_vis',
+  packages = ['swan_vis'],
+  version = '0.0.1',
+  license='MIT',  description = 'swan is a tool for visualizing and analyzing transcript isoforms',
+  author = 'Fairlie Reese',
+  author_email = 'fairlie.reese@gmail.com',
+  url = 'https://github.com/fairliereese/swan_vis/',
+  download_url = 'https://github.com/fairliereese/swan_vis/archive/v0.0.1-alpha.tar.gz',
+  keywords = ['swan', 'transcription', 'isoform'],
+  install_requires=[
+          'networkx',
+          'numpy',
+          'pandas',
+          'matplotlib',
+          'fpdf'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research ',    
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3.7',
+  ],
+)
