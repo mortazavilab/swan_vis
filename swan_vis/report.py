66c983e14626805db39231541e3f9a971d77169e
fairliereese <fairliek@comcast.net>
2020-03-27 12:38:52 -0700

    rolled back seaborn changes because i'm a strong independent woman who don't need no seaborn

--- swan_vis/report.py
+++ swan_vis/report.py
@@ -5,14 +5,11 @@ import matplotlib.pyplot as pyplot
 # report for genes - extension of FPDF class
 class Report(FPDF):
 
-	def __init__(self, prefix, gid, report_type, report_cols, header_cols, heatmap=False, num_transcripts=[]):
+	def __init__(self, prefix, report_type, report_cols, header_cols, heatmap=False):
 		super().__init__(orientation='L')
 
 		# change margins
 		self.set_margins(0.5, 0.5, 0.5)
-
-		# gene for this report
-		self.gid = gid
 
 		# set report type, 'browser' or 'swan'
 		if report_type != 'browser' and report_type != 'swan':
@@ -22,7 +19,6 @@ class Report(FPDF):
 
 		# does this report include a heatmap?
 		self.heatmap = heatmap
-		self.num_transcripts = num_transcripts
 
 		# the columns that we'll include
 		self.report_cols = report_cols
@@ -35,19 +31,10 @@ class Report(FPDF):
 		# color map in case we're making a heatmap
 		self.cmap = plt.get_cmap('Spectral_r')
 
-		# number of pages we've added
-		self.num_pages = 0
-
 	# header - should differ based on whether it's a browser report or
 	# a swan report
 	def header(self):
-
 		self.set_font('Arial', 'B', 10)
-
-		# increment the number of pages we've made
-		# get the number of transcripts on this page
-		num_transcripts = self.num_transcripts[self.num_pages]
-		self.num_pages += 1
 
 		# add extra room for the scale/colorbar if we're doing 
 		# the browser/heatmap version
@@ -67,7 +54,7 @@ class Report(FPDF):
 		self.cell(50, header_height, 'Transcript ID', border=True, align='C')
 
 		# in case we need to add the colorbar
-		colorbar_x = self.get_x() 
+		colorbar_x = self.get_x() + float(45/2) + 4.5
 		colorbar_y = self.get_y()
 
 		# dataset ID headers
@@ -76,34 +63,19 @@ class Report(FPDF):
 			self.cell(w_dataset, dataset_height, col,
 					  border=True, align='C')
 
-		# transcript model header
-		self.cell(100, header_height, 'Transcript Model', border=True, align='C')
-
-		# add colorbar and heatmap if we need to 
+		# add colorbar if we need to 
 		if self.heatmap == True: 
-
-			# colorbar
-			self.set_xy(50.5, 10.5)
-			self.cell(146, 10, border=True)
 			self.image(self.prefix+'_colorbar_scale.png',
-				x=colorbar_x+float(45/2) + 4.5, 
+				x=colorbar_x, 
 				y=colorbar_y+10,
 				w=90, h=135/14)
-
-			# heatmap
-			heatmap_name = '{}_{}_heatmap_{}.png'.format(self.prefix, self.gid, self.num_pages)
-			heatmap_height = num_transcripts*20
-			print(num_transcripts)
-			print(heatmap_height)
-			self.image(heatmap_name,
-				x=50.5,
-				y=20.5,
-				w=146,
-				h=heatmap_height)
 
 		# in case we need to add the browser scale
 		browser_scale_x = self.get_x()
 		browser_scale_y = self.get_y()
+
+		# transcript model header
+		self.cell(100, header_height, 'Transcript Model', border=True, align='C')
 
 		# add scale if we're doing browser models
 		if self.report_type == 'browser':
@@ -116,36 +88,43 @@ class Report(FPDF):
 	# add a transcript model to the report
 	def add_transcript(self, entry, oname):
 
+		# dynamically size dataset cols
+		w_dataset = 146/self.n_dataset_cols
+
 		self.set_font('Arial', '', 10)
 		self.cell(50, 20, entry['tid'], border=True, align='C')
+		for col in self.report_cols:
 
-		# tpm and presence/absence
-		if not self.heatmap:
-
-			# dynamically size dataset cols
-			w_dataset = 146/self.n_dataset_cols
-
-			for col in self.report_cols:
-
-				# fill with tpm number
-				if '_tpm' in col:
-					text = str(round(entry[col],2))
-
-				# presence/absence
-				else:
-					text = entry[col]
-					if text == True:
-						text = 'Yes'
-					elif text == False:
-						text = 'No'
-				self.cell(w_dataset, 20, text, border=True, align='C')	
-
-		# heatmap - don't need columns
-		else: 	
-			self.cell(146, 20)
-
+			# heat map coloring
+			if self.heatmap:
+				color = self.cmap(entry[col])
+				r = color[0]*255
+				b = color[1]*255
+				g = color[2]*255
+				self.set_fill_color(r,b,g)
+				border = False
+				fill = True
+				text = ''
+			# TPM	
+			elif '_tpm' in col:
+				text = str(round(entry[col],2))
+				border = True
+				fill = False
+			# presence/absence
+			else:
+				text = entry[col]
+				if text == True:
+					text = 'Yes'
+				elif text == False:
+					text = 'No'
+				border = True
+				fill = False 
+			self.cell(w_dataset, 20, text, border=border, align='C', fill=fill)	
 		x = self.get_x()
 		y = self.get_y()
+
+		# reset color to white
+		self.set_fill_color(255,255,255)
 
 		# embed transcript model
 		self.cell(100, 20, '', border=True)
@@ -157,5 +136,3 @@ class Report(FPDF):
 	def write_pdf(self, file):
 		with open(file, 'wb') as outfile:
 			outfile.write(self.output(dest='S').encode('latin-1'))
-
-
