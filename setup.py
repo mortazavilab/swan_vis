0f2dd1f1de35e453245fe8576b0937fd62290cc5
fairliereese <fairliek@comcast.net>
2020-05-29 10:07:20 -0700

    messing with setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'swan_vis',
   packages = ['swan_vis'],
-  version = '0.0.4_b',
+  version = '0.0.4',
   license='MIT',  description = 'swan is a tool for visualizing and analyzing transcript isoforms',
   author = 'Fairlie Reese',
   author_email = 'fairlie.reese@gmail.com',
@@ -18,7 +18,7 @@ setup(
           'fpdf',
           'seaborn',
           'diffxpy',
-          'tensorflow==2.2',
+          'tensorflow',
           'tensorflow-probability'
       ],
   classifiers=[
@@ -30,3 +30,14 @@ setup(
     'Programming Language :: Python :: 3.7',
   ],
 )
+
+try:
+  import networkx as nx 
+  import shutil
+  nx_loc = nx.__file__
+  nx_loc = '/'.join(nx_loc.split('/')[:-1])
+  nx_loc += '/drawing/nx_pylab.py'
+  nx_file = 'swan_vis/networkx_patch/nx_pylab.py'
+  shutil.copyfile(nx_file, nx_loc)
+except:
+  raise Exception('Unable to patch networkx to allow for dashed edges.')
