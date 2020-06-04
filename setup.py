af5fc0f4d53cdc53da6c111128b678d84f026150
fairliereese <fairliek@comcast.net>
2020-06-03 18:38:49 -0700

    working version of networkx patch

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'swan_vis',
   packages = ['swan_vis'],
-  version = '0.0.7',
+  version = '0.0.9',
   license='MIT',  description = 'swan is a tool for visualizing and analyzing transcript isoforms',
   author = 'Fairlie Reese',
   author_email = 'fairlie.reese@gmail.com',
   url = 'https://github.com/fairliereese/swan_vis/',
-  download_url = 'https://github.com/fairliereese/swan_vis/archive/0.0.7.tar.gz',
+  download_url = 'https://github.com/fairliereese/swan_vis/archive/0.0.9.tar.gz',
   keywords = ['swan', 'transcription', 'isoform', 'visualization'],
   install_requires=[
           'networkx',
