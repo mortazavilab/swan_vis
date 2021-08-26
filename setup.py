d3217aed07b74d1ddb6a81ddd073918f0d697c2a
fairliereese <fairliek@comcast.net>
2021-08-26 14:27:16 -0700

    updated install requirements

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
   name = 'swan_vis',
   packages = ['swan_vis'],
-  version = 'v1.0',
-  license='MIT', 
+  version = 'v2..0',
+  license='MIT',
   description = 'swan is a tool for visualizing and analyzing transcript isoforms',
   author = 'Fairlie Reese',
   author_email = 'fairlie.reese@gmail.com',
@@ -12,29 +12,24 @@ setup(
   keywords = ['swan', 'transcription', 'isoform', 'visualization'],
   python_requires='>=3.6',
   install_requires=[
-          'networkx',
-          'numpy',
-          'pandas',
-          'matplotlib',
-          'fpdf',
-          'seaborn',
-          'diffxpy',
-          'tensorflow',
-          'tensorflow-probability',
-	        'pathlib',
-          'tqdm'
+    fpdf,
+    matplotlib,
+    scipy,
+    statsmodels>=0.12.2,
+    numpy,
+    pandas>=1.1.0,
+    pytest,
+    networkx>=2.5,
+    tqdm,
+    anndata>=0.7,
+    diffxpy==0.7.4,
     ],
   classifiers=[
     'Development Status :: 3 - Alpha',
-    'Intended Audience :: Science/Research ',    
+    'Intended Audience :: Science/Research ',
     'Topic :: Scientific/Engineering :: Bio-Informatics',
-    'License :: OSI Approved :: MIT License', 
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.7',
-  ],
-      entry_points={
-        "console_scripts": [
-            'swan_patch_networkx=swan_vis.networkx_patch:main'
-        ]
-    }
+  ]
 )
